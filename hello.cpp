10a8f725717be5e6a3826aedda82eaf7a96469b3
trod33 <tyler_j_rodgers@yahoo.com>
2020-06-20 13:18:55 -0700

    Lab 1
    Hello World

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+//
+// hello.cpp
+//
+// Tyler Rodgers
+// 6/16/2020
+// Lab 1
+// Prints a greeting.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Greetings from Planet Earth!" << endl;
+ return 0;
+}
